b91fb5bfd2d86711a4d48aea06e8c4b9c9e75439
vashishtkumar <vashishtk270@gmail.com>
2025-04-29 12:20:06 +0530

    frontend8

--- frontend/src/components/ViewBookDetails/ViewBookDetails.jsx
+++ frontend/src/components/ViewBookDetails/ViewBookDetails.jsx
@@ -3,10 +3,18 @@ import axios from "axios";
 import Loader from "../Loader/Loader";
 import { useParams } from "react-router-dom";
 import { GrLanguage } from "react-icons/gr";
+import { FaHeart } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 const ViewBookDetails = () => {
   const { id } = useParams();
   const [Data, setData] = useState(null);
+
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const role = useSelector((state) => state.auth.role);
 
   useEffect(() => {
     const fetch = async () => {
@@ -21,10 +29,51 @@ const ViewBookDetails = () => {
   return (
     <>
       {Data && (
-        <div className="px-4 md:px-12 py-8 bg-zinc-900 flex flex-col md:flex-row gap-8">
-          <div className="bg-zinc-800 rounded p-4 h-[60vh] lg:h-[88vh] w-full lg:w-3/6 flex  items-center justify-center">
+        <div className="px-4 md:px-12 py-8 bg-zinc-900 flex flex-col lg:flex-row gap-8">
+          <div className="w-full lg:w-3/6 ">
             {/* */}
-            <img src={Data.url} alt="/" className="h-[50vh] lg:h-[70vh] rounded" />
+            <div className="flex flex-col lg:flex-row justify-around bg-zinc-800 p-12 rounded">
+              <img
+                src={Data.url}
+                alt="/"
+                className=" h-[50vh] md:h-[60vh] lg:h-[70vh] rounded"
+              />
+              {isLoggedIn === true && role === "user" && (
+                <div className="flex flex-row lg:flex-col items-center justify-between lg:justify-start mt-4 lg:mt-0">
+                  <button className="bg-white rounded lg:rounded-full text-3xl p-3 text-red-600 flex items-center justify-center">
+                    <FaHeart />
+                    <span className="ms-4 block lg:hidden">Favourites</span>
+                  </button>
+                  <button
+                    className="text-white rounded lg:rounded-full text-3xl mt-0 lg:mt-8 p-2 bg-blue-500 flex items-center justify-center"
+                    rounded-full
+                    text-2xl
+                    p-2
+                  >
+                    <FaShoppingCart />
+                    <span className="ms-4 block lg:hidden">Add to cart</span>
+                  </button>
+                </div>
+              )}
+
+              {isLoggedIn === true && role === "admin" && (
+                <div className="flex flex-row lg:flex-col items-center justify-between lg:justify-start mt-4 lg:mt-0">
+                  <button className="bg-white rounded lg:rounded-full text-3xl p-3 flex items-center justify-center">
+                    <FaEdit />
+                    <span className="ms-4 block lg:hidden">Edit</span>
+                  </button>
+                  <button
+                    className="text-red-500 rounded lg:rounded-full text-3xl mt-0 lg:mt-8 p-2 bg-white flex items-center justify-center"
+                    rounded-full
+                    text-2xl
+                    p-2
+                  >
+                    <MdDelete />
+                    <span className="ms-4 block lg:hidden">Delete Book</span>
+                  </button>
+                </div>
+              )}
+            </div>
           </div>
           <div className="p-4 w-full lg:w-3/6">
             <h1 className="text-4xl text-zinc-300 font-semibold">
